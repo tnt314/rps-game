2adac3bd6390cede1ef48be8cc79ffd2f2fcc512
Trong <34520239+tnt314@users.noreply.github.com>
2022-10-31 12:41:30 -0400

    Write playRound function

--- rps.js
+++ rps.js
@@ -29,4 +29,37 @@ function getComputerChoice() {
     }
 }
 
+// Single round
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Its a tie!"
+    }
+    else if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection === "scissors") {
+            return "You win! Rock beats Scissors";
+        }
+        else {
+            return "You lose! Paper beats Rock";
+        }
+    }
+    else if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection === "paper") {
+            return "You win! Scissors beats paper";
+        }
+        else {
+            return "You lose! Rock beats Scissors";
+        }
+    }
+    else if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats Rock";
+        }
+        else {
+            return "You lose! Scissors beats paper";
+        }
+    }
+}
 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
